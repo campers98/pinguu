2687bb6f3256792393d8c9de7223bdcaed8ca66c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-01-18 23:49:22 +0530

    🥀

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 from os import getenv
+
 TOKEN = getenv("TOKEN", None)
-MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority")
