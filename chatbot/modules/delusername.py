bc969edc38b6d6871ed6e077d2c89133e46e4530
Abishnoi69 <Abishnoi69@gmail.com>
2023-02-01 16:21:38 +0000

    🤦

--- chatbot/modules/delusername.py
+++ chatbot/modules/delusername.py
@@ -1,39 +1,36 @@
-import telegram.ext as tg
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import (
-    ContextTypes,
-    CommandHandler,
-    filters,
-    MessageHandler,
-)
-from telegram.constants import ChatType, ParseMode
-from telegram.helpers import mention_html
 import re
+
+from telegram import Update
+from telegram.ext import ContextTypes, MessageHandler, filters
+
 from chatbot import rani
+
 
 async def stop_chat_username(update: Update, context: ContextTypes.DEFAULT_TYPE):
     bot = context.bot
     chat = update.effective_chat
     message = update.effective_message
     users = update.effective_user
-    links = re.findall(r'@[^\s]+', message.text)
+    links = re.findall(r"@[^\s]+", message.text)
     if not links:
         return
     chat_admins = await rani.bot.getChatAdministrators(chat.id)
     admin_list = [x.user.id for x in chat_admins]
     if users.id in admin_list:
-       return
+        return
     if (await chat.get_member(bot.id)).can_delete_messages:
-       if message.text:
-          for link in links:
-             try:
-                 user = await bot.get_chat(link)
-                 print(user.id)
-                 if len(str(user.id)) > 12:
-                    await message.reply_text(f"{users.first_name}, ʏᴏᴜʀ ᴍᴇssᴀɢᴇ ᴡᴀs ʜɪᴅᴅᴇɴ, ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇs ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ.")
-                    await message.delete()
-             except:
-                 return
+        if message.text:
+            for link in links:
+                try:
+                    user = await bot.get_chat(link)
+                    print(user.id)
+                    if len(str(user.id)) > 12:
+                        await message.reply_text(
+                            f"{users.first_name}, ʏᴏᴜʀ ᴍᴇssᴀɢᴇ ᴡᴀs ʜɪᴅᴅᴇɴ, ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇs ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ."
+                        )
+                        await message.delete()
+                except:
+                    return
 
 
 USER = 110
