e76744bdec52fa16c5ae8c4800d531eb7ea8039e
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-06 12:37:35 +0530

    Added load and no load

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 from os import getenv
 
+
+LOAD = getenv("LOAD", "").split()
+
+NO_LOAD = getenv("NO_LOAD", "").split()
+
 TOKEN = getenv("TOKEN", None)
+
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
     "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority",
