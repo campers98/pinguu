573c6b772ccb3894b5c1202ef5cc1f405692876c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-02-08 11:20:54 +0530

    added help cmd

--- AsuX/modules/Start.py
+++ AsuX/modules/Start.py
@@ -22,10 +22,35 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         ]
     )
     await msg.reply_text(
+        f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n",
+        reply_markup=InlineKeyboardMarkup(akboss),
+    )
+
+   
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat = update.effective_chat
+    msg = update.effective_message
+    akboss = []
+    akboss.append(
+        [
+            InlineKeyboardButton(
+                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ➕",
+                url=f"http://t.me/{context.bot.username}?startgroup=true",
+            )
+        ]
+    )
+    await msg.reply_text(
         f"ʜᴇʏᴀ\nɪ'ᴍ {context.bot.first_name}\nɪ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴀᴄᴛɪᴠᴇ ʏᴏᴜʀ ᴄʜᴀᴛ \n\nɪ ᴄᴀɴ ʀᴇꜱᴛʀɪᴄᴛ ᴡʜɪᴄʜ ᴄᴏɴᴛᴀɪɴꜱ ᴘᴜʙʟɪᴄ ᴄʜᴀᴛ ᴜꜱᴇʀɴᴀᴍᴇ ᴍᴇꜱꜱᴀɢᴇꜱ",
         reply_markup=InlineKeyboardMarkup(akboss),
     )
 
+    
+    
+    
 
 START = CommandHandler(["chatbot", "start"], start, block=False)
+HELP = CommandHandler(["chelp", "help"], help, block=False)
+
 rani.add_handler(START)
+rani.add_handler(HELP)
