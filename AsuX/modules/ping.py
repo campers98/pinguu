0996d4f8114a354656c1d7bf2c1da457b1beb81a
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-10 16:35:36 +0530

    Create ping.py

---
+++ AsuX/modules/ping.py
@@ -0,0 +1,19 @@
+import time
+
+from Abg.helpers.human_read import get_readable_time
+from pyrogram import Client
+from pyrogram.types import Message
+
+from AsuX import uptime, Abishnoi
+
+
+@Abishnoi.on_cmd(["ping", "pong", "alive"])
+async def ping(self: Client, ctx: Message):
+    currentTime = get_readable_time(time.time() - uptime)
+    start_t = time.time()
+    rm = await ctx.reply_msg("💫 ᴘᴏɴɢ !!...")
+    end_t = time.time()
+    time_taken_s = round(end_t - start_t, 3)
+    await rm.edit_msg(
+        f"<b>ᴘɪɴɢ:</b> <code>{time_taken_s}ᴍs</code>\n<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{currentTime}</code>"
+    )
