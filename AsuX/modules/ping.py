98a374f3bbf2785fe11aa9ce9a7bf205987d4a5e
Abishnoi69 <Abishnoi69@gmail.com>
2023-07-10 13:55:19 +0000

    Aᴜᴛᴏ-ғɪx

--- AsuX/modules/ping.py
+++ AsuX/modules/ping.py
@@ -4,8 +4,8 @@ from Abg.helpers.human_read import get_readable_time
 from pyrogram import Client
 from pyrogram.types import Message
 
-from AsuX.modules.utils.langs import lang
 from AsuX import Abishnoi, StartTime
+from AsuX.modules.utils.langs import lang
 
 
 @Abishnoi.on_cmd(["ping", "pong", "alive"])
@@ -16,6 +16,4 @@ async def ping(self: Client, ctx: Message, _):
     rm = await ctx.reply_msg("_[ping_1]")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
-    await rm.edit_msg(
-        "_[ping_2]".format(time_taken_s, currentTime)
-    )
+    await rm.edit_msg("_[ping_2]".format(time_taken_s, currentTime))
