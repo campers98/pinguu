1f0721ed4d5d8aee4180bce3303ee0d21e8b773c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-14 09:29:28 +0530

    .

--- AsuX/modules/ping.py
+++ AsuX/modules/ping.py
@@ -1,19 +1,21 @@
 import time
 
 from Abg.helpers.human_read import get_readable_time
+#from Abg.helpers.ratelimit import ratelimiter
 from pyrogram import Client
 from pyrogram.types import Message
 
-from AsuX import Abishnoi, StartTime
-from AsuX.modules.utils.langs import lang
+from AsuX import StartTime, Abishnoi
 
 
 @Abishnoi.on_cmd(["ping", "pong", "alive"])
-@lang
-async def ping(self: Client, ctx: Message, _):
+#@ratelimiter
+async def ping(self: Client, ctx: Message):
     currentTime = get_readable_time(time.time() - StartTime)
     start_t = time.time()
-    rm = await ctx.reply_msg("_[ping_1]")
+    rm = await ctx.reply_msg("💫 ᴘᴏɴɢ !!...")
     end_t = time.time()
     time_taken_s = round(end_t - start_t, 3)
-    await rm.edit_msg("_[ping_2]".format(time_taken_s, currentTime))
+    await rm.edit_msg(
+        f"<b>ᴘɪɴɢ:</b> <code>{time_taken_s}ᴍs</code>\n<b>ᴜᴘᴛɪᴍᴇ:</b> <code>{currentTime}</code>"
+    )
